634ce24664061b40a6061addab1fded7d45a9b1a
Sebastian <hageneuer@gmail.com>
2024-06-24 21:18:30 +0200

    Responsive Design and Footer
    Adapted the responsive design again to fit smaller phones and prepared the footer a bit

--- footer.php
+++ footer.php
@@ -13,18 +13,13 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			© DiKopA Projekt <?php echo date("Y"); ?> |
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'dikopa-theme' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'dikopa-theme' ), 'WordPress' );
-				?>
-			</a>
+			© DiKopA Projekt <?php echo date("Y"); ?>
 			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'dikopa-theme' ), 'dikopa-theme', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
+			Kontakt
+			<span class="sep"> | </span>
+			Impressum
+			<span class="sep"> | </span>
+			Datenschutzbestimmungen
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->

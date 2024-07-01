5f031ac5d6d54daa4722057353ce01b48e1b70e6
Sebastian <hageneuer@gmail.com>
2024-07-01 21:10:56 +0200

    Build Veranstaltungen and Pages
    Veranstaltungen now shows all the Veranstaltungen in the future and in the past. Pages are styled a bit more, but still not finished.

---
+++ page-veranstaltungen.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package dikopa-theme
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+		<?php
+		$args = array(
+		    'post_type' => 'post',
+		    'post_status' => 'published',
+		    'category_name' => 'Vortragsankündigung',
+		);
+		$arr_posts = new WP_Query( $args );
+
+		if ( $arr_posts->have_posts() ) :
+		?>
+			<div class="loop-title">
+				<h1>Vergangene und anstehende Veranstaltungen</h1>
+			</div>
+		<?php
+		    while ( $arr_posts->have_posts() ) :
+		        $arr_posts->the_post();
+
+						/*
+						 * Include the Post-Type-specific template for the content.
+						 * If you want to override this in a child theme, then include a file
+						 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
+						 */
+						get_template_part( 'template-parts/content', get_post_type() );
+
+		    endwhile;
+				the_posts_navigation();
+		    wp_reset_postdata();
+
+		else :
+
+			get_template_part( 'template-parts/content', 'none' );
+
+		endif;
+		?>
+	</main><!-- #main -->
+
+<?php
+get_sidebar();
+get_footer();
--- template-parts/content.php
+++ template-parts/content.php
@@ -15,11 +15,8 @@
 	</div>
 	<div class="post-entry-container">
 		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
+		the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+
 		if ( 'post' === get_post_type() ) :
 		?>
 		<div class="entry-meta">

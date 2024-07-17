349d274d33ae704c8fb7f7dcc94d775bfce0bfe9
Sebastian <hageneuer@gmail.com>
2024-07-17 16:04:43 +0200

    Pages
    Updated pages to have a sidebar and proper title.

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -12,37 +12,17 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class('dikopa-single'); ?>>
 	<?php dikopa_theme_post_thumbnail(); ?>
 
-	<header class="entry-header">
+	<div class="loop-title loop-title-bottom">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+	</div>
 
-	<div class="entry-content">
-		<?php
-		the_content();
-
-		?>
-	</div><!-- .entry-content -->
-
-	<?php if ( get_edit_post_link() ) : ?>
-		<footer class="entry-footer">
+	<div class="post-entry-container">
+		<div class="entry-content">
 			<?php
-			edit_post_link(
-				sprintf(
-					wp_kses(
-						/* translators: %s: Name of current post. Only visible to screen readers */
-						__( 'Edit <span class="screen-reader-text">%s</span>', 'dikopa-theme' ),
-						array(
-							'span' => array(
-								'class' => array(),
-							),
-						)
-					),
-					wp_kses_post( get_the_title() )
-				),
-				'<span class="edit-link">',
-				'</span>'
-			);
+			the_content();
+
 			?>
-		</footer><!-- .entry-footer -->
-	<?php endif; ?>
+		</div><!-- .entry-content -->
+	</div><!-- .post-entry-container -->
+
 </article><!-- #post-<?php the_ID(); ?> -->
